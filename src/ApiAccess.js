a951997fc508037f2376aa7c001160c329236e3f
Timneji <139955017+timseoaudit@users.noreply.github.com>
2023-09-27 03:30:51 +0100

    add ID and Secret

--- src/ApiAccess.js
+++ src/ApiAccess.js
@@ -1,7 +1,7 @@
 import {useState, useEffect} from 'react';
 
-const CLIENT_ID = 'YOUR_CLIENT_ID';
-const CLIENT_SECRET = 'YOUR_CLIENT_SECRET';
+const CLIENT_ID = '1a672fd1f3014899a3c1fb50aac6b4e7';
+const CLIENT_SECRET = 'd60be553af1e4cc389f9380b6111b6a9';
 
 function ApiFetch() {
 const [token, setToken] = useState('');
